ab0fd05396c0ef3cb1d58a3497f4ac8ca132cddd
Rafael Moura <rafael.mesquita.moura@gmail.com>
2024-09-27 12:29:48 -0300

    feat: Include ELB DNS

--- login.js
+++ login.js
@@ -9,8 +9,8 @@ module.exports.login = async (event) => {
 
 function init(username) {
     var options = {
-        host: 'localhost',
-        port: '30000',
+        host: 'http://a8fa75205608242ebaff09f4ff8cb7c3-1746343393.us-east-1.elb.amazonaws.com',
+        port: '80',
         path: `/api/clientes?cpf=${username}`,
         method: 'GET'
     };
@@ -21,6 +21,8 @@ function init(username) {
             console.log('Usuário não encontrado');
             return;
         }
+
+        console.log('Response from Server: ' + res);
         
         //chamada ao próximo AWS Lambda
     });
